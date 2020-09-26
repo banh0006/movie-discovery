aebbf310bc4a77a58f9a348294c22ad27500c302
banh0006 <banh0006@algonquinlive.com>
2020-09-26 09:33:09 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-discovery/precache-manifest.fa38175232b5c556f42945c5805a1b92.js"
+  "/movie-discovery/precache-manifest.0da083a4bfde18c48250907adf957d1a.js"
 );
 
 self.addEventListener('message', (event) => {
