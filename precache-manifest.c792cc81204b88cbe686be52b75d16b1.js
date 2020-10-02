a74f05fd735cd9ff61f375706eddd8a0e17a6b5a
banh0006 <banh0006@algonquinlive.com>
2020-10-02 12:20:21 -0400

    Updates

---
+++ precache-manifest.c792cc81204b88cbe686be52b75d16b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c85d84e2a05c1ce9fbca00752b2736",
+    "url": "/movie-discovery/index.html"
+  },
+  {
+    "revision": "5553d06fedc8df83ab05",
+    "url": "/movie-discovery/static/css/2.a2211c7b.chunk.css"
+  },
+  {
+    "revision": "2befbb12668fa816f72a",
+    "url": "/movie-discovery/static/css/main.dc27ac7d.chunk.css"
+  },
+  {
+    "revision": "5553d06fedc8df83ab05",
+    "url": "/movie-discovery/static/js/2.4d69757a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/movie-discovery/static/js/2.4d69757a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2befbb12668fa816f72a",
+    "url": "/movie-discovery/static/js/main.3102ed1d.chunk.js"
+  },
+  {
+    "revision": "c1d29d6ee8a8d5beb35b",
+    "url": "/movie-discovery/static/js/runtime-main.a6258db4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-discovery/precache-manifest.3c6601de13cd4c189911f84062d64538.js"
+  "/movie-discovery/precache-manifest.c792cc81204b88cbe686be52b75d16b1.js"
 );
 
 self.addEventListener('message', (event) => {
