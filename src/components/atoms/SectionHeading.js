f5a40692049d4e284ac116b2e3e5a227f1560e32
banh0006 <banh0006@algonquinlive.com>
2020-10-02 14:38:43 -0400

    change web background color

--- src/components/atoms/SectionHeading.js
+++ src/components/atoms/SectionHeading.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 export default function SectionHeading(props) {
+    const styles = {
+        sectionTitle : {
+            color: '#fff'
+        }
+    }
     return (
         <div className="section-heading">
-            <h2 className="section-title">{props.sectionTitle}</h2>
+            <h2 className="section-title" style={styles.sectionTitle} >{props.sectionTitle}</h2>
         </div>
     )
 }
